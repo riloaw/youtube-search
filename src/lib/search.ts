ae540fd2711b9ade840f493ed98e7cf7b398bf7b
Rizal Loa Wanda <riloaw@gmail.com>
2021-12-11 02:59:04 +0700

    Update search.ts

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -9,7 +9,7 @@ export async function searchVideo(searchQuery: string) {
   let fetched = false;
   const options = { type: "video", limit: 0 };
 
-  const searchRes: any = await got.get(encodeURI(`${YOUTUBE_URL}/results?q=${encodeURI(searchQuery.trim())}&hl=id&sp=CAMSAhAB`));
+  const searchRes: any = await got.get(encodeURI(`${YOUTUBE_URL}/results?q=${encodeURI(searchQuery.trim())}&hl=id&sp=CAASAhAB`));
   let html = await searchRes.body;
   // try to parse html
   try {
